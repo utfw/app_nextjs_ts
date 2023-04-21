3099bb8b54fc34044f81e2cc39c3540de2fdc78b
utfw <hwan.c.0330@gmail.com>
2023-04-21 10:49:35 +0900

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
+import Header from '@/component/header'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Header />
+      <Component {...pageProps} />
+    </div>
+  )
+
 }
+
+/* 
+Component: 현재 페이지를 의미, 페이지 변경 시 해당 Component 는 변경
+pageProps:DataFatching 메소드를 이용해 미리 가져온 초기 객체. 
+
+_app.tsx, _document.tsx는 서버에서만 실행됨. 
+*/
