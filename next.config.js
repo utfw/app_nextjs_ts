520af0715fc9ce4e62f1655ff9ac69ca2a182da6
utfw <hwan.c.0330@gmail.com>
2023-04-21 13:09:48 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const debug = process.env.NODE_ENV !== 'production'
-const name = 'app_nextjs_ts'
+const name = 'nextjs_ts_test' // github이름이 들어가야함.
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: !debug ? `${name}` : '',
+  assetPrefix: !debug ? `/${name}/` : '',
   basePath: '/app_nextjs_ts', 
   trailingSlash: true,
 }
 
 module.exports = nextConfig
-
