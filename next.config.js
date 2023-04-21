ac9b8faee143dd5b885f02dfcdc59c12a08085cd
utfw <127266193+utfw@users.noreply.github.com>
2023-04-21 12:31:44 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== 'production'
+const name = 'app_nextjs_ts'
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: !debug ? `${name}` : '',
+  basePath: '/app_nextjs_ts', 
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
